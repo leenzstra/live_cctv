ac10014c1082f5aa1c2ae00a61161b2f7b16678a
lolomarka <lolomarka@gmail.com>
2022-04-20 12:32:46 +0500

    args to client3

--- client3.py
+++ client3.py
@@ -2,11 +2,28 @@ import cv2
 import socket
 import math
 import pickle
+import signal
 import sys
+import argparse
 
 max_length = 65000
-host = "localhost"
-port = 5000
+
+
+def signal_handler(sig, frame):
+    logging.info('You pressed Ctrl+C!')
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--server-ip", default='127.0.0.1',
+                help="ip address of the server to which the client will connect")
+ap.add_argument("-p", "--server-port", default=5000)
+args = vars(ap.parse_args())
+
+host = args['server_ip']
+port = int(args['server_port'])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -23,32 +40,21 @@ while ret:
         buffer = buffer.tobytes()
         # get size of the frame
         buffer_size = len(buffer)
-
         num_of_packs = 1
         if buffer_size > max_length:
             num_of_packs = math.ceil(buffer_size/max_length)
-
-        frame_info = {"packs":num_of_packs}
-
-        # send the number of packs to be expected
-        print("Number of packs:", num_of_packs)
+        frame_info = {"packs": num_of_packs}
         sock.sendto(pickle.dumps(frame_info), (host, port))
-        
+
         left = 0
         right = max_length
 
         for i in range(num_of_packs):
-            #print("left:", left)
-            #print("right:", right)
-
-            # truncate data to send
             data = buffer[left:right]
             left = right
             right += max_length
-
-            # send the frames accordingly
             sock.sendto(data, (host, port))
-    
+
     ret, frame = cap.read()
 
 print("done")
